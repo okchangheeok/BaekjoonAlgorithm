3299181713966c0fec09519e60b7a75b07eac024
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2024-03-18 03:25:33 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
+//이동 횟수 계산 후 출력 함수
+//N부터 역방향으로 1까지 가면서 매 회 (전 단계 횟수) * 2 + 1 진행.
 void print_count(int N, int answer = 1){
   if(N <= 1){
     cout << answer << "\n";
@@ -10,6 +11,8 @@ void print_count(int N, int answer = 1){
   print_count(N - 1, 2 * answer + 1);
 }
 
+//이동 경로 출력 함수
+//N부터 역방향으로 1까지 가면서 매 회 전 단계 과정 출력, A/C 출력, 전 단계 과정 출력 진행.
 void print_process(int N, int A = 1, int B = 2, int C = 3){
   if(N <= 1){
     cout << A << " " << C << "\n";
@@ -27,7 +30,7 @@ int main() {
 
   int N;
   cin >> N;
-  
+
   print_count(N);
   print_process(N);
   return 0;
