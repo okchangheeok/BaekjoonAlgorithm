1a3b0ba978d7949aebc87f6e56dda4a9a909f6da
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2024-03-18 01:04:54 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 124 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*
+void print_count(int N, int answer = 1){
+  if(N <= 1){
+    cout << answer << "\n";
+    return;
+  }
+  print_count(N - 1, 2 * answer + 1);
+}*/
+void print_process(int N, int A = 1, int B = 2, int C = 3){
+  if(N <= 1){
+    cout << A << " " << C << "\n";
+    return;
+  }
+
+  print_process(N - 1, A, C, B);
+  cout << A << " " << C << "\n";
+  print_process(N - 1, B, A, C);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int N;
+  int count;
+
+  cin >> N;
+  count = pow(2, N) - 1;
+
+  cout << count << "\n";
+  print_process(N);
+  return 0;
+}
