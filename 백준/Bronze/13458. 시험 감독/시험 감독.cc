882caf8f4fe5f0ad48c812c39399312d77547b58
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2024-03-20 18:43:40 +0900

    [Bronze II] Title: 시험 감독, Time: 116 ms, Memory: 5800 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N;
+  cin >> N;
+  int A[N];
+  int B;
+  int C;
+  long long int answer;
+
+  for (int i = 0; i < N; i++)
+    cin >> A[i];
+  cin >> B;
+  cin >> C;
+
+  answer = (long long int)N; //시험장 수 = 총감독관 수 미리 고려
+  for (int i = 0; i < N; i++) {
+    if (A[i] > B) {             //총 감독관으로 감당 불가 시
+      answer += (long long int)((A[i] - B) / C); //부감독관으로 나눈 몫을 answer에 더함
+      if ((A[i] - B) % C)       //나머지 있을 시 한명 더 더함
+        answer++;
+    }
+  }
+
+  cout << answer;
+
+  return 0;
+}
