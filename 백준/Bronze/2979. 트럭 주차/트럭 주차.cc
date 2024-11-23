69f577fc80f47b3593f6fbbfabee7392838e226a
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2024-11-23 22:58:14 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -2,26 +2,24 @@
 
 using namespace std;
 
-int t[100], A, B, C, t1, t2, ans;
+int t[104], A, B, C, t1, t2, ans;
 
 int main(){
 	cin >> A >> B >> C;
-	B *= 2;
-	C *= 3;
-	//visited를 넣어도 되겠 
-	for(int i = 0; i < 3; i++){
+	
+    for(int i = 0; i < 3; i++){
 		cin >> t1 >> t2;
 		for(int j = t1; j < t2; j++)
 			t[j]++;
 	}
 	
 	for(int i = 0; i < 100; i++){
-		if(t[i] == 1)
+        if(t[i] == 1)
 			ans += A;
 		else if(t[i] == 2)
-			ans += B;
+			ans += B * 2;
 		else if(t[i] == 3)
-			ans += C; 
+			ans += C * 3; 
 	}
 
 	cout << ans; 
