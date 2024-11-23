8f24cbfb70a59b30a8e041a2876f056f76b8509d
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2024-11-23 22:43:34 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
-using namespace std;
-int price[3];
-int ttmm[2];
-map<int, int> mp;
-int ret;
 
-int main()
-{
-    for(int i = 0; i < 3; i++){
-        cin >> price[i];
-        price[i] *= (i + 1);
-    }
-    
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++)
-            cin >> ttmm[j];
-        for(int j = ttmm[0]; j < ttmm[1]; j++)
-            mp[j]++;
-    }
-    
-    for(auto i: mp)
-        ret += price[i.second - 1];
-        
-    cout << ret;
-    
-    return 0;
+using namespace std;
+
+int t[100], A, B, C, t1, t2, ans;
+
+int main(){
+	cin >> A >> B >> C;
+	B *= 2;
+	C *= 3;
+	//visited를 넣어도 되겠 
+	for(int i = 0; i < 3; i++){
+		cin >> t1 >> t2;
+		for(int j = t1; j < t2; j++)
+			t[j]++;
+	}
+	
+	for(int i = 0; i < 100; i++){
+		if(t[i] == 1)
+			ans += A;
+		else if(t[i] == 2)
+			ans += B;
+		else if(t[i] == 3)
+			ans += C; 
+	}
+
+	cout << ans; 
+	
+	return 0;
 }
