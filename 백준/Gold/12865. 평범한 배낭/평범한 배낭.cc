8894f007624dc6523f0c69a8b41ceb880fff6c72
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-02-18 01:25:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -4,20 +4,15 @@ using namespace std;
 
 int N, K;
 int w, v, ans[100005];
-vector<pair<int, int>> vec;
 
 int main()
 {
     cin >> N >> K;
     for(int i = 0; i < N; i++){
     	cin >> w >> v;
-    	vec.push_back({w, v});
+    	for(int j = K; w <= j; j--)
+    		ans[j] = max(ans[j], ans[j - w] + v);
     }
 	
-	sort(vec.begin(), vec.end());
-	
-	for(auto it: vec)
-		for(int j = K; it.first <= j; j--)
-    		ans[j] = max(ans[j], ans[j - it.first] + it.second);
 	cout << ans[K];
 }
