1c8483decbd070b7bf152326499b5a9e813c8f17
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-03-04 20:12:44 +0900

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -7,15 +7,12 @@ string s;
 vector<string> vs;
 
 bool compare(string a, string b){
-	if(a.size() != b.size())
-		return a.size() < b.size();
-	else{
-		for(int i = 0; i < a.size(); i++){
+	if(a.size() == b.size())
+		for(int i = 0; i < a.size(); i++)
 			if(a[i] != b[i])
 				return (a[i] < b[i]);
-		}
-		return 1;
-	}
+		
+	return a.size() < b.size();
 }
 
 int main()
@@ -27,7 +24,7 @@ int main()
     		if(flag == 0 && s[i] == '0'){
     			if(!('0' <= s[i + 1] && s[i + 1] <= '9'))
     				vs.push_back("0");
-    		}
+            }
 			else if(flag == 0 && '1' <= s[i] && s[i] <= '9'){
     			flag = 1;
     			b = i;
@@ -48,9 +45,8 @@ int main()
 	
 	sort(vs.begin(), vs.end(), compare);
 	
-	for(auto it: vs){
+	for(auto it: vs)
 		cout << it << "\n";
-	}
 
     return 0;
 }
