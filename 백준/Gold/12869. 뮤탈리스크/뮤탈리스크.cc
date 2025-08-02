8521880cc24579c668dc4772d0d587dc07205454
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-02 15:41:26 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3092 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,45 +1,54 @@
-#include <bits/stdc++.h> 
-using namespace std;
-int a[3], n, visited[64][64][64]; 
-int _a[6][3] = {
-	{9, 3, 1}, 
-	{9, 1, 3}, 
-	{3, 1, 9}, 
-	{3, 9, 1}, 
-	{1, 3, 9}, 
-	{1, 9, 3}
-};
-struct A{
-    int a, b, c; 
-}; 
-queue<A>q; 
+#include <iostream>
+#include <queue>
 
-int solve(int a, int b, int c){
-    visited[a][b][c] = 1; 
-    q.push({a, b, c}); 
-    while(q.size()){
-        int a = q.front().a; 
-        int b = q.front().b; 
-        int c = q.front().c; 
-        q.pop();
-        if(visited[0][0][0]) break;
-        for(int i = 0; i < 6; i++){
-            int na = max(0, a - _a[i][0]);
-            int nb = max(0, b - _a[i][1]); 
-            int nc = max(0, c - _a[i][2]); 
-            if(visited[na][nb][nc]) continue;
-            visited[na][nb][nc] = visited[a][b][c] + 1;  
-            q.push({na, nb, nc}); 
-        }
-    }
-    return visited[0][0][0] - 1;   
+using namespace std;
+
+int visited[65][65][65];
+struct trio{
+	int i, j, k;
+};
+
+const int dmg[6][3] = 
+{{9, 3, 1},
+{9, 1, 3},
+{3, 9, 1},
+{3, 1, 9},
+{1, 9, 3},
+{1, 3, 9}};
+
+int bfs(int a, int b, int c){
+	queue<trio> q;
+	q.push({a, b, c});
+	visited[a][b][c] = 1;
+	while(!q.empty()){
+		int x = q.front().i;
+		int y = q.front().j;
+		int z = q.front().k;
+		q.pop();
+		for(int i = 0; i < 6; i++){
+			int nx = max(0, x - dmg[i][0]);
+			int ny = max(0, y - dmg[i][1]);
+			int nz = max(0, z - dmg[i][2]);
+			
+			if(nx == 0 && ny == 0 && nz == 0)
+				return visited[x][y][z];
+			if(visited[nx][ny][nz])
+				continue;
+
+			q.push({nx, ny, nz});
+			visited[nx][ny][nz] = visited[x][y][z] + 1;
+		}
+	}
 }
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-	cin >> n; 
-	for(int i = 0; i < n; i++) cin >> a[i]; 
-	cout << solve(a[0], a[1], a[2]) << "\n"; 
-    return 0;
+
+int main(){
+	int N;
+	int cd[3] = {0, 0, 0};
+	
+	cin >> N;
+	
+	for(int i = 0; i < N; i++) cin >> cd[i];
+	
+	cout << bfs(cd[0], cd[1], cd[2]);
+	return 0;
 }
