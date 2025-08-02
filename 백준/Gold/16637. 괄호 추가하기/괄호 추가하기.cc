a1f3741ac92d78f545f80928aaeafd2c83064ec0
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-02 23:32:13 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;  
+
+vector<int> num; 
+vector<char> oper_str; 
+int n, ret = -1 * pow(2, 31);
+string s;
+
+int oper(char a, int b, int c){
+    if(a == '+') return b + c; 
+    if(a == '-') return b - c; 
+    if(a == '*') return b * c;  
+} 
+
+void go(int here, int _num){
+    if(here == num.size() - 1){ 
+        ret = max(ret, _num); 
+        return;
+    }  
+    go(here + 1, oper(oper_str[here], _num, num[here + 1]));
+
+    if(here + 2 <= num.size() - 1){
+        int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]); 
+        go(here + 2, oper(oper_str[here], _num, temp));  
+    } 
+    return;
+} 
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL); 
+    cin >> n >> s;
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0)num.push_back(s[i] - '0');
+        else oper_str.push_back(s[i]);
+    } 
+    go(0, num[0]);  
+    cout << ret << "\n";
+    return 0;
+} 
