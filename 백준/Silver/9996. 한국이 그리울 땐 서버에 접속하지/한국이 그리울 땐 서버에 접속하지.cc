111a2e78a029d1a3ce4ae20a93efefaef01ffc9a
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-09 00:07:39 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,40 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int N;
-string A1;
-string tmp;
-vector<string> vec_std;
-vector<string> vec;
-queue<string> ret;
-
-vector<string> split(string str){
-    vector<string> ret;
-    ret.push_back(str.substr(0, str.find("*")));
-    ret.push_back(str.substr(str.find("*") + 1, str.size() - str.find("*") + 1));
-    
-    return ret;
-}
-
-int main()
-{
-    cin >> N;
-    cin >> A1;
-    vec_std = split(A1);
-    for(int i = 0; i < N; i++){
-        cin >> tmp;
-        vec.push_back(tmp);
-        if(vec[i].size() < A1.size() - 1)
-            ret.push("NE");
-        else if((vec[i].substr(0, vec_std[0].size()) == vec_std[0]) && (vec[i].substr(vec[i].size() - vec_std[1].size()) == vec_std[1]))
-            ret.push("DA");
-        else
-            ret.push("NE");
-    }
-    
-    while(ret.size()){
-        cout << ret.front() << '\n';
-        ret.pop();
-    }
+#include<bits/stdc++.h> 
+using namespace std;   
+int n; 
+string s, ori_s, pre, suf; 
+int main(){
+    cin >> n;
+    cin >> ori_s;  
+    int pos = ori_s.find('*');  
+    pre = ori_s.substr(0, pos); 
+    suf = ori_s.substr(pos + 1); 
+    for(int i = 0; i < n; i++){
+        cin >> s; 
+        if(pre.size() + suf.size() > s.size()){
+            cout << "NE\n";
+        }else{
+            if(pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA\n";
+            else cout <<"NE\n";  
+        } 
+    } 
     return 0;
-}
+} 
