fcbd8d9ded1e37a3b6fac1c2dfb9470c81041e37
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-18 22:41:46 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,27 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-typedef long long ll;
-ll n, rmn, sum, ans;
+int n, D = 1, tmp, ans;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-    while(cin >> n){
-    	rmn = 1 % n;
-    	sum = rmn;
-    	ans = 1;
-    	while(1){
-    		if(sum % n == 0)
-    			break;
-    		rmn = (10 * rmn) % n;
-    		sum += rmn;
-    		sum %= n;
-    		ans++;
+int main(){
+	ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+	
+	
+	while(cin >> n){
+		while(1){
+			D %= n;
+			ans++;
+			if(D == 0) break;
+			else D = D * 10 + 1;
 		}
-		cout << ans << '\n';
-    }
+		cout << ans << "\n";
+		
+		D = 1;
+		ans = 0;
+	}
+	
 	return 0;
 }
-
