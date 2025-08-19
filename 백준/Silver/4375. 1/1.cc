0962293541a0ffa31efbaeeed084012891c10edd
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-19 22:08:33 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-int n, D = 1, tmp, ans;
+int n, D = 1, ans;
 
-int main(){
-	ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-	
-	
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+  	
 	while(cin >> n){
-		while(1){
-			D %= n;
-			ans++;
-			if(D == 0) break;
-			else D = D * 10 + 1;
+  		while(D){
+  			D %= n;
+  			ans++;
+  			
+  			if(D == 0)	break;
+  			else		D = D * 10 + 1;
 		}
+		
 		cout << ans << "\n";
 		
-		D = 1;
-		ans = 0;
+  		D = 1; ans = 0;
 	}
-	
+  	
 	return 0;
 }
