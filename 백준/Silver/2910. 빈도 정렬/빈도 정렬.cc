33611368c9405e679fa8110eae35802b05be2154
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-08-24 20:00:52 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,36 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-long N, C, tmp;
-map<long, long> mp;
-vector<pair<long, long>> v;
-vector<int> num;
+long long N, C, tmp;
 
-bool compare(pair<long, long>& a, pair<long, long>& b)
-{
-    if(a.first != b.first)
-	    return a.first > b.first;
-	else
-	    return find(num.begin(), num.end(), a.second) < find(num.begin(), num.end(), b.second);
+map<long long, int> m;
+map<long long, int> loc;
+
+vector<pair<long long, int>> ans;
+
+bool compare(const pair<long long, int> a, const pair<long long, int> b){
+	if(a.second != b.second) return a.second > b.second;
+	else return loc[a.first] < loc[b.first];
 }
 
 int main()
 {
-    cin >> N >> C;
-    
-    for(int i = 0; i < N; i++){
-        cin >> tmp;
-        mp[tmp]++;
-        if(find(num.begin(), num.end(), tmp) == num.end())
-            num.push_back(tmp);
-    }
-    for(long it: num){
-        v.push_back({mp[it], it});
-    }
-    sort(v.begin(), v.end(), compare);
-    for(int i = 0; i < v.size(); i++)
-        for(int j = 0; j < v[i].first; j++)
-            cout << v[i].second << " ";
+	cin >> N >> C;
+	for(int i = 1; i <= N; i++){
+		cin >> tmp;
+		m[tmp]++;
+		if(loc.find(tmp) == loc.end()) loc[tmp] = i;
+	}
+	
+	for(auto it: m)
+		ans.push_back({it.first, it.second});
+	
+	sort(ans.begin(), ans.end(), compare);
+	
+	for(auto it: ans)
+		for(int i = 0; i < it.second; i++)
+			cout << it.first << " ";
+	
     return 0;
 }
