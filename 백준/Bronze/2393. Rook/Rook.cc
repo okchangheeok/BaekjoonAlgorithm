aa753e33ff64c474bd67d6bfac7db54163883da9
okchangheeok <149135459+okchangheeok@users.noreply.github.com>
2025-09-14 22:40:16 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+	
+cout << "  ___  ___  ___\n";
+cout << "  | |__| |__| |\n";
+cout << "  |           |\n";
+cout << "   \\_________/\n";
+cout << "    \\_______/\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |     |\n";
+cout << "     |_____|\n";
+cout << "  __/       \\__\n";
+cout << " /             \\\n";
+cout << "/_______________\\\n";
+	
+    return 0;
+}
